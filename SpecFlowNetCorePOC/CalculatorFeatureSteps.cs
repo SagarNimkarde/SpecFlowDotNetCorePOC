d471196045e92373b9e72263a392c99fb4896d22
snimkarde <snimkarde@TAVLAPT155>
2018-04-06 15:05:26 +0530

    First commit

---
+++ SpecFlowNetCorePOC/CalculatorFeatureSteps.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowNetCorePOC
+{
+    [Binding]
+    public class CalculatorFeatureSteps
+    {
+        int result = 0;
+        int nextNumber = 0;
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            nextNumber = p0;
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            result = result + nextNumber;
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            
+        }
+    }
+}
